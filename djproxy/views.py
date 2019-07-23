b19b02f440885ae4ab11e8ec7044e12812d53e28
Asher Foa <1268088+asherf@users.noreply.github.com>
2019-07-23 13:12:57 -0700

    Remove workaround for a django bug, this django version is no loner supported.

--- djproxy/views.py
+++ djproxy/views.py
@@ -1,5 +1,4 @@
 """HTTP Reverse Proxy class based generic view."""
-from django import get_version as get_django_version
 from django.http import HttpResponse
 from django.views.generic import View
 from requests import request
@@ -64,12 +63,6 @@ class HttpProxy(View):
         """Retrieve the upstream content and build an HttpResponse."""
         headers = self.request.headers.filter(self.ignored_request_headers)
         qs = self.request.query_string if self.pass_query_string else ''
-
-        # Fix for django 1.10.0 bug https://code.djangoproject.com/ticket/27005
-        if (self.request.META.get('CONTENT_LENGTH', None) == '' and
-                get_django_version() == '1.10'):
-            del self.request.META['CONTENT_LENGTH']
-
         request_kwargs = self.middleware.process_request(
             self, self.request, method=self.request.method, url=self.proxy_url,
             headers=headers, data=self.request.body, params=qs,
